1dcdb7c2f449217a48e9a5835bbc6645d24f97fd
Kiev Da Silva Maia <kiev.maia@portoseguro.com.br>
2025-08-12 18:50:10 -0300

    Criação da Rich Domain Model para a classe Customer

---
+++ src/main/java/com/algaworks/algashop/ordering/domain/entity/Customer.java
@@ -0,0 +1,177 @@
+package com.algaworks.algashop.ordering.domain.entity;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.time.OffsetDateTime;
+import java.util.Objects;
+import java.util.UUID;
+
+public class Customer implements Serializable {
+    private UUID id;
+    private String fullName;
+    private LocalDate birthDate;
+    private String email;
+    private String phone;
+    private String document;
+    private Boolean promotionNotificationAllowed;
+    private Boolean archived;
+    private OffsetDateTime registeredAt;
+    private OffsetDateTime archivedAt;
+    private Integer loyaltyPoints;
+
+    public Customer(UUID id, String fullName, LocalDate birthDate, String email, String phone, String document,
+                    Boolean promotionNotificationAllowed, Boolean archived, OffsetDateTime registeredAt,
+                    OffsetDateTime archivedAt, Integer loyaltyPoints) {
+        this.id = id;
+        this.fullName = fullName;
+        this.birthDate = birthDate;
+        this.email = email;
+        this.phone = phone;
+        this.document = document;
+        this.promotionNotificationAllowed = promotionNotificationAllowed;
+        this.archived = archived;
+        this.registeredAt = registeredAt;
+        this.archivedAt = archivedAt;
+        this.loyaltyPoints = loyaltyPoints;
+    }
+
+    public Customer(UUID id, String fullName, LocalDate birthDate, String email, String phone, String document,
+                    Boolean promotionNotificationAllowed, OffsetDateTime registeredAt) {
+        this.id = id;
+        this.fullName = fullName;
+        this.birthDate = birthDate;
+        this.email = email;
+        this.phone = phone;
+        this.document = document;
+        this.promotionNotificationAllowed = promotionNotificationAllowed;
+        this.registeredAt = registeredAt;
+    }
+
+    public void addLoyaltyPoints(Integer loyaltyPoints) {
+        this.loyaltyPoints = loyaltyPoints;
+    }
+
+    public void archived(Boolean archived) {
+        this.archived = archived;
+    }
+
+    public void enablePromotionNotification() {
+        this.setPromotionNotificationAllowed(true);
+    }
+
+    public void disablePromotionNotification() {
+        this.setPromotionNotificationAllowed(false);
+    }
+
+    public void changeFullName(String fullName) {
+        this.setFullName(fullName);
+    }
+
+    public void changeEmail(String email) {
+        this.setEmail(email);
+    }
+
+    public void changePhone(String phone) {
+        this.setPhone(phone);
+    }
+
+    public UUID id() {
+        return id;
+    }
+
+    public String fullName() {
+        return fullName;
+    }
+
+    public LocalDate birthDate() {
+        return birthDate;
+    }
+
+    public String email() {
+        return email;
+    }
+
+    public String phone() {
+        return phone;
+    }
+
+    public String document() {
+        return document;
+    }
+
+    public Boolean isPromotionNotificationAllowed() {
+        return promotionNotificationAllowed;
+    }
+
+    public Boolean isArchived() {
+        return archived;
+    }
+
+    public OffsetDateTime registeredAt() {
+        return registeredAt;
+    }
+
+    public OffsetDateTime archivedAt() {
+        return archivedAt;
+    }
+
+    public Integer loyaltyPoints() {
+        return loyaltyPoints;
+    }
+
+    private void setId(UUID id) {
+        this.id = id;
+    }
+
+    private void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    private void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    private void setEmail(String email) {
+        this.email = email;
+    }
+
+    private void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    private void setDocument(String document) {
+        this.document = document;
+    }
+
+    private void setPromotionNotificationAllowed(Boolean promotionNotificationAllowed) {
+        this.promotionNotificationAllowed = promotionNotificationAllowed;
+    }
+
+    private void setArchived(Boolean archived) {
+        this.archived = archived;
+    }
+
+    private void setRegisteredAt(OffsetDateTime registeredAt) {
+        this.registeredAt = registeredAt;
+    }
+
+    private void setArchivedAt(OffsetDateTime archivedAt) {
+        this.archivedAt = archivedAt;
+    }
+
+    private void setLoyaltyPoints(Integer loyaltyPoints) {
+        this.loyaltyPoints = loyaltyPoints;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Customer customer = (Customer) o;
+        return Objects.equals(id, customer.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
