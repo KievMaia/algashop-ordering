4ffa829a8723d6ecabb09c0172c3f213824f3c26
Kiev Da Silva Maia <kiev.maia@portoseguro.com.br>
2025-11-19 17:12:20 -0300

    Implementa a validação de alteração do status.

--- src/main/java/com/algaworks/algashop/ordering/domain/entity/OrderStatusEnum.java
+++ src/main/java/com/algaworks/algashop/ordering/domain/entity/OrderStatusEnum.java
@@ -1,9 +1,27 @@
 package com.algaworks.algashop.ordering.domain.entity;
 
+import java.util.Arrays;
+import java.util.List;
+
 public enum OrderStatusEnum {
     DRAFT,
-    PLACED,
-    PAID,
-    READY,
-    CANCELED
+    PLACED(DRAFT),
+    PAID(PLACED),
+    READY(PAID),
+    CANCELED(PAID, READY, PLACED, DRAFT);
+
+    OrderStatusEnum(OrderStatusEnum...  previousStatuses) {
+        this.previousStatuses = Arrays.asList(previousStatuses);
+    }
+
+    private final List<OrderStatusEnum> previousStatuses;
+
+    public boolean canChangeTo(OrderStatusEnum newStatus) {
+        var currentStatus = this;
+        return newStatus.previousStatuses.contains(currentStatus);
+    }
+
+    public boolean canNotChangeTo(OrderStatusEnum newStatus) {
+        return !canChangeTo(newStatus);
+    }
 }
