3127c0d3db410f3f88a2586999c304a5e5505300
Kiev Da Silva Maia <kiev.maia@portoseguro.com.br>
2025-12-29 17:24:41 -0300

    Aplica padrão nested

--- src/test/java/com/algaworks/algashop/ordering/domain/entity/OrderChangingTest.java
+++ src/test/java/com/algaworks/algashop/ordering/domain/entity/OrderChangingTest.java
@@ -4,106 +4,97 @@ import com.algaworks.algashop.ordering.domain.exception.OrderCannotBeEditedExcep
 import com.algaworks.algashop.ordering.domain.valueobject.Quantity;
 import com.algaworks.algashop.ordering.domain.valueobject.id.CustomerId;
 import org.assertj.core.api.Assertions;
-import org.assertj.core.api.ThrowableAssert;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static com.algaworks.algashop.ordering.domain.entity.OrderStatusEnum.CANCELED;
 import static com.algaworks.algashop.ordering.domain.entity.OrderStatusEnum.PAID;
 import static com.algaworks.algashop.ordering.domain.entity.OrderStatusEnum.PLACED;
-import static com.algaworks.algashop.ordering.domain.entity.OrderStatusEnum.READY;
 
 public class OrderChangingTest {
 
-    @Test
-    public void givenDraftOrder_WhenTryModifiedItems_ShouldAllowChange() {
-        var order = Order.draft(new CustomerId());
+    @Nested
+    @DisplayName("Given a DRAFT order")
+    class DraftOrder {
 
-        var product = ProductTestDataBuilder.aProductAltMousePad().build();
+        private Order order;
 
-        order.addItem(product, new Quantity(3));
+        @BeforeEach
+        void setUp() {
+            order = Order.draft(new CustomerId());
+        }
 
-        Assertions.assertThat(order.items()).isNotEmpty();
+        @Test
+        @DisplayName("Should allow adding items")
+        void shouldAllowAddItem() {
+            var product = ProductTestDataBuilder.aProductAltMousePad().build();
+            order.addItem(product, new Quantity(3));
+            Assertions.assertThat(order.items()).isNotEmpty();
+        }
+
+        @Test
+        @DisplayName("Should allow changing shipping address")
+        void shouldAllowChangeShipping() {
+            var shipping = OrderTestDataBuilder.aShipping();
+            order.changeShipping(shipping);
+            Assertions.assertThat(order.shipping().address().street()).isEqualTo(shipping.address().street());
+        }
+
+        @Test
+        @DisplayName("Should allow changing billing address")
+        void shouldAllowChangeBilling() {
+            var billingAlt = OrderTestDataBuilder.aBillingAlt();
+            order.changeBilling(billingAlt);
+            Assertions.assertThat(order.billing().address().street()).isEqualTo(billingAlt.address().street());
+        }
+
+        @Test
+        @DisplayName("Should allow changing payment method")
+        void shouldAllowChangePaymentMethod() {
+            var method = PaymentMethodEnum.CREDIT_CARD;
+            order.changePaymentMethod(method);
+            Assertions.assertThat(order.paymentMethod()).isEqualTo(method);
+        }
     }
 
-    @Test
-    public void givenDraftOrder_WhenTryModifiedShippingAddress_ShouldAllowChange() {
-        var order = Order.draft(new CustomerId());
-        var shipping = OrderTestDataBuilder.aShipping();
-        order.changeShipping(shipping);
+    @Nested
+    @DisplayName("Given an order that is NOT in DRAFT")
+    class NonDraftOrder {
 
-        Assertions.assertThat(order.shipping().address().street()).isEqualTo(shipping.address().street());
-    }
+        @Test
+        @DisplayName("Should block changes when status is PLACED")
+        void shouldBlockWhenPlaced() {
+            var order = OrderTestDataBuilder.anOrder().orderStatusEnum(PLACED).build();
+            assertBlock(order);
+        }
 
-    @Test
-    public void givenDraftOrder_WhenTryModifiedBillingAddress_ShouldAllowChange() {
-        var order = Order.draft(new CustomerId());
-        var billingAlt = OrderTestDataBuilder.aBillingAlt();
-        order.changeBilling(billingAlt);
+        @Test
+        @DisplayName("Should block changes when status is PAID")
+        void shouldBlockWhenPaid() {
+            var order = OrderTestDataBuilder.anOrder().orderStatusEnum(PAID).build();
+            assertBlock(order);
+        }
 
-        Assertions.assertThat(order.billing().address().street()).isEqualTo(billingAlt.address().street());
-    }
+        @Test
+        @DisplayName("Should block changes when status is CANCELED")
+        void shouldBlockWhenCanceled() {
+            var order = OrderTestDataBuilder.anOrder().orderStatusEnum(CANCELED).build();
+            assertBlock(order);
+        }
 
-    @Test
-    public void givenDraftOrder_WhenTryModifiedPaymentMethod_ShouldAllowChange() {
-        var order = Order.draft(new CustomerId());
-        var paymentMethodEnum = PaymentMethodEnum.CREDIT_CARD;
-        order.changePaymentMethod(paymentMethodEnum);
+        @Test
+        @DisplayName("Should block changes after transitioning from DRAFT to PLACED")
+        void shouldBlockAfterTransition() {
+            var order = OrderTestDataBuilder.anOrder().build();
+            order.place();
+            assertBlock(order);
+        }
 
-        Assertions.assertThat(order.paymentMethod().name()).isEqualTo(paymentMethodEnum.name());
-    }
-
-    @Test
-    public void givenOrderPlaced_WhenTryModifiedItems_ShouldNotAllow() {
-        var order = OrderTestDataBuilder.anOrder().orderStatusEnum(PLACED).build();
-
-        ThrowableAssert.ThrowingCallable addItemTask = () -> order.addItem(
-                ProductTestDataBuilder.aProductUnavailable().build(), new Quantity(1));
-
-        Assertions.assertThatExceptionOfType(OrderCannotBeEditedException.class)
-                .isThrownBy(addItemTask);
-    }
-
-    @Test
-    public void givenOrderPaid_WhenTryModifiedItems_ShouldNotAllow() {
-        var order = OrderTestDataBuilder.anOrder().orderStatusEnum(PAID).build();
-
-        ThrowableAssert.ThrowingCallable addItemTask = () -> order.addItem(
-                ProductTestDataBuilder.aProductUnavailable().build(), new Quantity(1));
-
-        Assertions.assertThatExceptionOfType(OrderCannotBeEditedException.class)
-                .isThrownBy(addItemTask);
-    }
-
-    @Test
-    public void givenOrderReady_WhenTryModifiedItems_ShouldNotAllow() {
-        var order = OrderTestDataBuilder.anOrder().orderStatusEnum(READY).build();
-
-        ThrowableAssert.ThrowingCallable addItemTask = () -> order.addItem(
-                ProductTestDataBuilder.aProductUnavailable().build(), new Quantity(1));
-
-        Assertions.assertThatExceptionOfType(OrderCannotBeEditedException.class)
-                .isThrownBy(addItemTask);
-    }
-
-    @Test
-    public void givenOrderCancelled_WhenTryModifiedItems_ShouldNotAllow() {
-        var order = OrderTestDataBuilder.anOrder().orderStatusEnum(CANCELED).build();
-
-        ThrowableAssert.ThrowingCallable addItemTask = () -> order.addItem(
-                ProductTestDataBuilder.aProductUnavailable().build(), new Quantity(1));
-
-        Assertions.assertThatExceptionOfType(OrderCannotBeEditedException.class)
-                .isThrownBy(addItemTask);
-    }
-
-    @Test
-    public void givenOrder_WhenStatusChangesFromDraftToPlaced_ThenChangesShouldBeBlocked() {
-        var order = OrderTestDataBuilder.anOrder().build();
-        order.place();
-
-        Assertions.assertThatExceptionOfType(OrderCannotBeEditedException.class)
-                .isThrownBy(() -> order.changeShipping(OrderTestDataBuilder.aShippingAlt()))
-                .withMessageContaining(order.id().value().toString())
-                .withMessageContaining(PLACED.name());
+        private void assertBlock(Order order) {
+            Assertions.assertThatExceptionOfType(OrderCannotBeEditedException.class)
+                    .isThrownBy(() -> order.addItem(ProductTestDataBuilder.aProductAltMousePad().build(), new Quantity(1)));
+        }
     }
 }
