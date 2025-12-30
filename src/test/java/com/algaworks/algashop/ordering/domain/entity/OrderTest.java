f07f885ed7f66396a6b370e599df48a181d3a8a2
Kiev Da Silva Maia <kiev.maia@portoseguro.com.br>
2025-12-30 11:39:22 -0300

    Organiza os testes.

--- src/test/java/com/algaworks/algashop/ordering/domain/entity/OrderTest.java
+++ src/test/java/com/algaworks/algashop/ordering/domain/entity/OrderTest.java
@@ -66,43 +66,6 @@ class OrderTest {
     }
 
     @Test
-    public void shouldRemoveItem() {
-        var order = Order.draft(new CustomerId());
-
-        var productMouse = ProductTestDataBuilder.aProductAltMousePad().build();
-        var productMemory = ProductTestDataBuilder.aProductAltRamMemory().build();
-
-        order.addItem(productMouse, new Quantity(1));
-        order.addItem(productMemory, new Quantity(1));
-
-        Assertions.assertThat(order.items().size()).isEqualTo(2);
-        Assertions.assertThat(order.totalAmount()).isEqualTo(new Money("300"));
-
-        var orderItemId = order.items().stream().filter(item ->
-                item.productId().equals(productMouse.id())).findFirst();
-        order.removeItem(orderItemId.get().id());
-
-        Assertions.assertThat(order.items().size()).isEqualTo(1);
-        Assertions.assertThat(order.totalAmount()).isEqualTo(new Money("200"));
-    }
-
-    @Test
-    public void shouldTryRemoveANonexistentItem() {
-        var order = Order.draft(new CustomerId());
-
-        Assertions.assertThatExceptionOfType(OrderDoesNotContainOrderItemException.class)
-                .isThrownBy(() -> order.removeItem(new OrderItemId()));
-    }
-
-    @Test
-    public void shouldTryRemoveAPlacedOrderItem() {
-        var order = OrderTestDataBuilder.anOrder().orderStatusEnum(PLACED).build();
-
-        Assertions.assertThatExceptionOfType(OrderCannotBeEditedException.class)
-                .isThrownBy(() -> order.removeItem(new OrderItemId()));
-    }
-
-    @Test
     public void shouldGenerateExceptionWhenTryToChangeItemSet() {
         var order = Order.draft(new CustomerId());
 
