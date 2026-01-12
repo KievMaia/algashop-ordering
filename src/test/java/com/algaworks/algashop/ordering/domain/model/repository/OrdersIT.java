c9a9488110c617a5a9410eaa5a73b52bc2894020
Kiev Da Silva Maia <kiev.maia@portoseguro.com.br>
2026-01-12 17:51:38 -0300

    Retira import sem uso.

--- src/test/java/com/algaworks/algashop/ordering/domain/model/repository/OrdersIT.java
+++ src/test/java/com/algaworks/algashop/ordering/domain/model/repository/OrdersIT.java
@@ -2,7 +2,6 @@ package com.algaworks.algashop.ordering.domain.model.repository;
 
 import com.algaworks.algashop.ordering.domain.model.entity.order.OrderTestDataBuilder;
 import com.algaworks.algashop.ordering.infrastructure.persistence.provider.OrdersPersistenceProvider;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
