f3f7ca49cd1f27a645106d1b05cfe81b2a3f9639
Kiev Da Silva Maia <kiev.maia@portoseguro.com.br>
2026-01-19 13:46:10 -0300

    Retira import sem uso.

--- src/main/java/com/algaworks/algashop/ordering/infrastructure/persistence/provider/OrdersPersistenceProvider.java
+++ src/main/java/com/algaworks/algashop/ordering/infrastructure/persistence/provider/OrdersPersistenceProvider.java
@@ -5,7 +5,6 @@ import com.algaworks.algashop.ordering.domain.model.repository.Orders;
 import com.algaworks.algashop.ordering.domain.model.valueobject.id.OrderId;
 import com.algaworks.algashop.ordering.infrastructure.persistence.assembler.OrderPersistenceEntityAssembler;
 import com.algaworks.algashop.ordering.infrastructure.persistence.disassembler.OrderPersistenceEntityDisassembler;
-import com.algaworks.algashop.ordering.infrastructure.persistence.entity.OrderPersistenceEntity;
 import com.algaworks.algashop.ordering.infrastructure.persistence.repository.OrderPersistenceEntityRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
