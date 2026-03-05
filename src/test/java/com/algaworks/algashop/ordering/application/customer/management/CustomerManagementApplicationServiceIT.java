cef7d11a7937ba2bbd921095f91d401ac6ec2948
KievMaia <kiev@prologapp.com>
2026-03-05 17:24:08 -0300

    Retira import sem uso.

--- src/test/java/com/algaworks/algashop/ordering/application/customer/management/CustomerManagementApplicationServiceIT.java
+++ src/test/java/com/algaworks/algashop/ordering/application/customer/management/CustomerManagementApplicationServiceIT.java
@@ -7,8 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.time.LocalDate;
-
 import static com.algaworks.algashop.ordering.application.customer.management.CustomerInputTestDateBuilder.aCustomer;
 import static com.algaworks.algashop.ordering.application.customer.management.CustomerUpdateInputTestDataBuilder.aCustomerUpdateInput;
 
