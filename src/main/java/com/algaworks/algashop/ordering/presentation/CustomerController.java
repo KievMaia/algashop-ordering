afde763a80cfaac3654dc8a8a5828137284c12f1
Kiev Da Silva Maia <kiev.maia@portoseguro.com.br>
2026-04-13 14:59:59 -0300

    Usando mock no teste de contrato.

--- src/main/java/com/algaworks/algashop/ordering/presentation/CustomerController.java
+++ src/main/java/com/algaworks/algashop/ordering/presentation/CustomerController.java
@@ -1,7 +1,10 @@
 package com.algaworks.algashop.ordering.presentation;
 
 import com.algaworks.algashop.ordering.application.customer.management.CustomerInput;
+import com.algaworks.algashop.ordering.application.customer.management.CustomerManagementApplicationService;
 import com.algaworks.algashop.ordering.application.customer.query.CustomerOutput;
+import com.algaworks.algashop.ordering.application.customer.query.CustomerQueryService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,30 +12,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.OffsetDateTime;
-import java.util.UUID;
-
 @RestController
 @RequestMapping("/api/v1/customers")
+@RequiredArgsConstructor
 public class CustomerController {
+
+    private final CustomerManagementApplicationService customerManagementApplicationService;
+    private final CustomerQueryService customerQueryService;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public CustomerOutput create(@RequestBody CustomerInput input) {
-        var address = input.getAddress();
-        return CustomerOutput.builder()
-                .id(UUID.randomUUID())
-                .phone(input.getPhone())
-                .email(input.getEmail())
-                .firstName(input.getFirstName())
-                .lastName(input.getLastName())
-                .birthDate(input.getBirthDate())
-                .document(input.getDocument())
-                .promotionNotificationsAllowed(input.getPromotionNotificationsAllowed())
-                .registeredAt(OffsetDateTime.now())
-                .archived(false)
-                .loyaltyPoints(0)
-                .address(address)
-                .build();
+        var customerId = customerManagementApplicationService.create(input);
+        return customerQueryService.findById(customerId);
     }
 }
